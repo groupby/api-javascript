f5b9a3f3d0fce52954f075d0f4672937d3886fbc
Ben Teichman <ben.l.teichman@gmail.com>
2016-08-30 20:03:06 -0400

    use strictly es5 in gulp tasks

--- build/tasks/docs.js
+++ build/tasks/docs.js
@@ -1,20 +1,20 @@
 var gulp = require('gulp'),
   typedoc = require('gulp-typedoc');
 
-gulp.task('docs', () => {
+gulp.task('docs', function() {
   return gulp.src(['src/**/*.ts'])
-   .pipe(typedoc({
-     module: 'commonjs',
-     target: 'es5',
-     includeDeclarations: true,
+    .pipe(typedoc({
+      module: 'commonjs',
+      target: 'es5',
+      includeDeclarations: true,
 
-     out: './docs',
-     mode: 'file',
-     includeDeclarations: false,
+      out: './docs',
+      mode: 'file',
+      includeDeclarations: false,
 
-     name: 'groupby-api',
-     ignoreCompilerErrors: true,
-     excludeExternals: true,
-     version: true
-   }));
+      name: 'groupby-api',
+      ignoreCompilerErrors: true,
+      excludeExternals: true,
+      version: true
+    }));
 });
