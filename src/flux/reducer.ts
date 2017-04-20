91fe7ba13593e071c885ac69787284ed65d82d14
Victoria Johns <victoria.johns@groupbyinc.com>
2017-04-20 17:22:28 -0400

    Add boilerplate for reducers

--- src/flux/reducer.ts
+++ src/flux/reducer.ts
@@ -1,11 +1,83 @@
-import * as actions from './actions';
 import * as redux from 'redux';
+import Actions from './actions';
+import Store from './store';
 
-export function updateQuery(state, action) {
+export function updateQuery(state: Store.Query, action) {
   switch (action) {
-    case actions.UPDATE_QUERY:
-      return { ...state, query: action.query }
-      break;
+    case Actions.UPDATE_QUERY:
+      return { ...state, query: action.query };
+    default:
+      return state;
+  }
+}
+
+export function updateNavigations(state: Store.Navigation, action) {
+  switch (action) {
+    // case Actions.SELECT_REFINEMENT:
+    // return { ...state, query: action.navigationId };
+    default:
+      return state;
+  }
+}
+
+export function updateSort(state: Store.Sort, action) {
+  switch (action) {
+    // case Actions.UPDATE_QUERY:
+    //   return { ...state, query: action.query };
+    default:
+      return state;
+  }
+}
+
+export function updateProducts(state: Store.Product, action) {
+  switch (action) {
+    // case Actions.UPDATE_QUERY:
+    //   return { ...state, query: action.query };
+    default:
+      return state;
+  }
+}
+
+export function updateCollections(state: Store.Collection, action) {
+  switch (action) {
+    // case Actions.UPDATE_QUERY:
+    //   return { ...state, query: action.query };
+    default:
+      return state;
+  }
+}
+
+export function updateAutocomplete(state: Store.Autocomplete, action) {
+  switch (action) {
+    // case Actions.UPDATE_QUERY:
+    //   return { ...state, query: action.query };
+    default:
+      return state;
+  }
+}
+
+export function updateRedirect(state, action) {
+  switch (action) {
+    // case Actions.UPDATE_QUERY:
+    //   return { ...state, query: action.query };
+    default:
+      return state;
+  }
+}
+
+export function updateErrors(state, action) {
+  switch (action) {
+    // case Actions.UPDATE_QUERY:
+    //   return { ...state, query: action.query };
+    default:
+      return state;
+  }
+}
+
+export function updateWarnings(state, action) {
+  switch (action) {
+    // case Actions.UPDATE_QUERY:
+    //   return { ...state, query: action.query };
     default:
       return state;
   }
@@ -13,6 +85,14 @@ export function updateQuery(state, action) {
 
 export default redux.combineReducers({
   data: redux.combineReducers({
-    query: updateQuery()
+    query: updateQuery,
+    sort: updateSort,
+    products: updateProducts,
+    collections: updateCollections,
+    navigations: updateNavigations,
+    autocomplete: updateAutocomplete,
+    redirect: updateRedirect,
+    errors: updateErrors,
+    warnings: updateWarnings
   })
 });
