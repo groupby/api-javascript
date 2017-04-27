6708dff640ea6417f1ac225276799c9d6244846b
Ben Teichman <ben.l.teichman@gmail.com>
2017-04-27 16:33:34 -0400

    lowercase import array findIndex polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,12 +1,12 @@
 import * as arrayIncludes from 'array-includes';
 import * as arrayFind from 'array.prototype.find';
-import * as arrayFindIndex from 'array.prototype.findIndex';
+import * as arrayFindIndex from 'array.prototype.findindex';
 import * as objectAssign from 'es6-object-assign';
 import * as promise from 'es6-promise';
 import 'es6-symbol/implement';
 
-arrayFind.shim();
 arrayFindIndex.shim();
+arrayFind.shim();
 arrayIncludes.shim();
 objectAssign.polyfill();
 promise.polyfill();
