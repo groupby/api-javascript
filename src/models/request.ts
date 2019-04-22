38317e5a907f1dab787a43393938a8ef1f16da57
Joey Deol <35696810+JoeyDeol@users.noreply.github.com>
2019-04-22 17:01:23 -0400

    SF-1335: Add optional sessionId and visitorId to Request (#93)
    * Update Request interface to include optional sessionId and visitorId fields

--- src/models/request.ts
+++ src/models/request.ts
@@ -32,6 +32,10 @@ export interface Request {
     pruneRefinements: boolean;
     disableAutocorrection: boolean;
     wildcardSearchEnabled: boolean;
+
+    // tracking
+    sessionId?: string;
+    visitorId?: string;
 }
 
 export type Sort = FieldSort | ByIdSort;
